dfab600699312c203d19b483f5aafa093b65ded4
Vigneshkumar <vigneshzx@gmail.com>
2023-10-31 19:09:04 +0530

    furniture view added

---
+++ Furniture_demo_SwiftUI/Model/Furniture.swift
@@ -0,0 +1,18 @@
+//
+//  Furniture.swift
+//  Furniture_demo_SwiftUI
+//
+//  Created by vignesh kumar c on 31/10/23.
+//
+
+import SwiftUI
+
+struct Furniture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Furniture()
+}
